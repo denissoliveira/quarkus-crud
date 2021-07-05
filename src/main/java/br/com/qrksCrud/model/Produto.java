1e4455aee1398a71b5e67c37c5f137f0edd687d0
denissoliveira <denissoliveira@gmail.com>
2021-07-05 20:58:02 -0300

    feat: Criação do produto

---
+++ src/main/java/br/com/qrksCrud/model/Produto.java
@@ -0,0 +1,34 @@
+package br.com.qrksCrud.model;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntity;
+
+@Entity
+public class Produto extends PanacheEntity {
+
+    private String nome;
+    private BigDecimal valor;
+    @CreationTimestamp private Date dataCriacao;
+    @UpdateTimestamp private Date dataAtualizacao;
+    
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public BigDecimal getValor() {
+		return valor;
+	}
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+    
+}
